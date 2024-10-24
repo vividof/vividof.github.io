20c882980538a97462cbc7c811ce557d262defde
that1guy24 <vivid.of31@gmail.com>
2024-10-23 19:02:47 -0700

    oo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // This is correct for username.github.io
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
