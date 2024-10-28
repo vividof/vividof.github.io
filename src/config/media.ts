c41f344be3cf798f6efcfeda7956a5138ce1f9f9
that1guy24 <vivid.of31@gmail.com>
2024-10-28 13:25:25 -0700

    Update media.ts

--- src/config/media.ts
+++ src/config/media.ts
@@ -5,7 +5,12 @@ export const mediaConfig = {
       '/videos/background2.mp4',
       '/videos/background3.mp4',
       '/videos/background4.mp4',
-      '/videos/background5.mp4'
+      '/videos/background5.mp4',
+      '/videos/background6.mp4',
+      '/videos/background7.mp4',
+      '/videos/background8.mp4',
+      '/videos/background9.mp4',
+      '/videos/background10.mp4'
     ],
     fallbackImage: '/images/background.jpg',
   },
